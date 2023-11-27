5105f33dafd429fac812fb9bb49bd8ad61bcba9a
Alejandro Pedraza <luisalejandropedrazacubillos@student.mitt.ca>
2023-11-27 10:15:13 -0600

    Improve the validation and add a confirmation modal

--- assets/script/index.js
+++ assets/script/index.js
@@ -20,23 +20,29 @@ const advice = select('.advice')
 const divContacts = select('.output')
 const input = select('input[type=text]')
 const addBtn = select('.add-button')
+const yesBtn = select('.yes-button')
+const noBtn = select('.no-button')
+const confirmationModal = select ('.confirmation-modal')
 
 function validation() {
     let inputValue = input.value.split(', ');
-    if (
-        !inputValue[2].includes('@') || 
-        !inputValue[2].includes('.com')
-        ) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(inputValue)) {
         advice.textContent = 'Please enter a valid email or put it in the correct order'
         return false;
     } else {
         advice.textContent = 'Contact Created'
         return true;
     }
+
+    
 }
 
 function listContacts() {
     let inputValues = input.value.split(', ');
+    let upperCase = inputValues[1].charAt(0).toUpperCase() + inputValues[1].slice(1);
+
     if (!validation(inputValues)) {
         return false;
     }
@@ -45,9 +51,16 @@ function listContacts() {
     const contactDiv = document.createElement('div')
     contactDiv.className = 'field'
     divContacts.appendChild(contactDiv)
-    contactDiv.innerHTML = `<b>Name:</b> ${contact.name}<br><b>City:</b> ${contact.city}<br><b>Email:</b> ${contact.email}`;
+    contactDiv.innerHTML = `<b>Name:</b> ${contact.name}<br><b>City:</b> ${upperCase}<br><b>Email:</b> ${contact.email}`;
 
-    onEvent(contactDiv, 'click', () => {divContacts.removeChild(contactDiv)})
+    onEvent(contactDiv, 'click', () => {
+        confirmationModal.style.display = 'grid';
+        onEvent(yesBtn, 'click', () => {
+            divContacts.removeChild(contactDiv)
+            confirmationModal.style.display = 'none'
+        })
+        onEvent(noBtn, 'click', () => {confirmationModal.style.display = 'none';})
+    })
 }
 
 onEvent(addBtn, 'click', listContacts)
